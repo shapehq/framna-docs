b3f5d5f3b8d164aeaac0e10b87492f0eb935df18
Simon Støvring <mail@simonbs.dk>
2023-10-13 10:27:55 +0200

    Removes unused code

--- __test__/mock/MockDateProvider.ts
+++
@@ -1,5 +0,0 @@
-import { IDateProvider } from "@/lib/date/IDateProvider"
-
-export class MockDateProvider implements IDateProvider {
-  now = new Date()
-}
--- src/lib/date/DateProvider.ts
+++
@@ -1,7 +0,0 @@
-import { IDateProvider } from "./IDateProvider"
-
-export class DateProvider implements IDateProvider {
-  get now(): Date {
-    return new Date()
-  }
-}
