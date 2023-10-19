2c14e3b48c8b7ce7382e8a07301a7def407c605e
Simon Støvring <mail@simonbs.dk>
2023-10-20 00:58:34 +0200

    Handles empty project list

--- src/features/projects/view/ProjectList.tsx
+++ src/features/projects/view/ProjectList.tsx
@@ -1,4 +1,4 @@
-import { List } from "@mui/material"
+import { List, Box, Typography } from "@mui/material"
 import ProjectListItem from "./ProjectListItem"
 import ProjectListItemPlaceholder from "./ProjectListItemPlaceholder"
 import IProject from "../domain/IProject"
@@ -17,25 +17,40 @@ const ProjectList = <ProjectType extends IProject>(
   }: ProjectListProps<ProjectType>
 ) => {
   const loadingItemCount = 6
-  return (
-    <List
-      disablePadding
-      sx={{ width: "100%", height: "100%" }}
-    >
-      {isLoading && 
-        [...new Array(loadingItemCount)].map((_, index) => (
-          <ProjectListItemPlaceholder key={index}/>
-        ))
-      }
-      {!isLoading && projects.map(project => (
-        <ProjectListItem
-          key={project.id}
-          project={project}
-          isSelected={project.id === selectedProjectId}
-        />
-      ))}
-    </List>
-  )
+  if (isLoading || projects.length > 0) {
+    return (
+      <List
+        disablePadding
+        sx={{ width: "100%", height: "100%" }}
+      >
+        {isLoading && 
+          [...new Array(loadingItemCount)].map((_, index) => (
+            <ProjectListItemPlaceholder key={index}/>
+          ))
+        }
+        {!isLoading && projects.map(project => (
+          <ProjectListItem
+            key={project.id}
+            project={project}
+            isSelected={project.id === selectedProjectId}
+          />
+        ))}
+      </List>
+    )
+  } else {
+    return (
+      <Box sx={{
+        display: "flex",
+        justifyContent: "center",
+        padding: "15px",
+        marginTop: "15px"
+      }}>
+        <Typography variant="h5" align="center">
+          Your list of projects is empty.
+        </Typography>
+      </Box>
+    )
+  }
 }
 
 export default ProjectList
