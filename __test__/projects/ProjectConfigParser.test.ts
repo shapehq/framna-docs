8796dd4edcfe3a0df6f400182ba703039059b115
Simon Støvring <mail@simonbs.dk>
2023-10-20 00:14:31 +0200

    Fixes import

--- __test__/projects/ProjectConfigParser.test.ts
+++ __test__/projects/ProjectConfigParser.test.ts
@@ -1,4 +1,4 @@
-import ProjectConfigParser from "@/features/projects/domain/ProjectConfigParser"
+import ProjectConfigParser from "../../src/features/projects/domain/ProjectConfigParser"
 
 test("It parses an empty string", async () => {
   const sut = new ProjectConfigParser()
