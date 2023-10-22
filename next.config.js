9f413c70d2cdf63d27a9ddf15bfcbf07ecb0d5d1
Simon Støvring <mail@simonbs.dk>
2023-10-22 15:03:00 +0200

    Configures ESLint (#17)
    * Configures ESLint
    
    * Disable linting during build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eslint: {
+    // Allows production builds to successfully complete even if it has linting errors.
+    // This is only OK bevause we do linting as part of our CI setup.
+    ignoreDuringBuilds: true,
+  }
+}
 
 module.exports = nextConfig
