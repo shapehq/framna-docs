c2e1b38dc548f6dff2c3df0d1d6bcd822794bd08
Simon Støvring <mail@simonbs.dk>
2023-10-23 09:10:48 +0200

    Handles Auth0 login errors by forcing user to logout

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,3 +1,21 @@
-import { handleAuth } from '@auth0/nextjs-auth0'
+import { NextRequest, NextResponse } from "next/server"
+import { handleAuth } from "@auth0/nextjs-auth0"
 
-export const GET = handleAuth()
+const {
+  AUTH0_ISSUER_BASE_URL
+} = process.env
+
+export const GET = handleAuth({
+  async onError(req: NextRequest) {
+    // If we encounter an error during login then we force the user
+    // to logout using Auth0's /oidc/logout endpoint as documented here:
+    // https://auth0.com/docs/authenticate/login/logout/log-users-out-of-auth0
+    // While the documentation states that an id_token_hint or logout_hint
+    // should be provided that is not needed as the user is not fully logged
+    // in at this point.
+    const url = new URL(AUTH0_ISSUER_BASE_URL + "/oidc/logout")
+    const redirectURI = req.nextUrl.protocol + "//" + req.nextUrl.host
+    url.searchParams.append("post_logout_redirect_uri", redirectURI)
+    return NextResponse.redirect(url)
+  }
+})
