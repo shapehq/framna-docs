aa985563a778abe8aee132b975b9828381d4b8ee
Simon Støvring <mail@simonbs.dk>
2023-10-23 12:04:13 +0200

    Handles Auth0 login errors by forcing user to logout (#31)
    * Handles Auth0 login errors by forcing user to logout
    
    * Moves force logout logic to /api/auth/forceLogout

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,3 +1,10 @@
-import { handleAuth } from '@auth0/nextjs-auth0'
+import { NextRequest, NextResponse } from "next/server"
+import { handleAuth } from "@auth0/nextjs-auth0"
 
-export const GET = handleAuth()
+export const GET = handleAuth({
+  async onError(req: NextRequest) {
+    const url = req.nextUrl.clone()
+    url.pathname = "/api/auth/forceLogout"
+    return NextResponse.redirect(url)
+  }
+})
