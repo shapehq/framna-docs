e0da1868c7120293d6fe9d5bcac65768b967b4ab
Simon Støvring <mail@simonbs.dk>
2023-10-24 10:50:26 +0200

    Validates OAuthToken schema with zod (#44)

--- __test__/auth/OAuthTokenCoder.test.ts
+++ __test__/auth/OAuthTokenCoder.test.ts
@@ -1,5 +1,36 @@
 import OAuthTokenCoder from "../../src/features/auth/domain/OAuthTokenCoder"
 
+test("It encodes a valid token", async () => {
+  const token = {
+    accessToken: "foo",
+    refreshToken: "bar",
+    accessTokenExpiryDate: new Date(new Date().getTime() + 3600 * 1000),
+    refreshTokenExpiryDate: new Date(new Date().getTime() + 24 * 3600 * 1000)
+  }
+  const str = OAuthTokenCoder.encode(token)
+  const decodedToken = JSON.parse(str)
+  expect(decodedToken.accessToken).toBe(token.accessToken)
+  expect(decodedToken.refreshToken).toBe(token.refreshToken)
+  expect(decodedToken.accessTokenExpiryDate).toEqual(token.accessTokenExpiryDate.toISOString())
+  expect(decodedToken.refreshTokenExpiryDate).toEqual(token.refreshTokenExpiryDate.toISOString())
+})
+
+test("It decodes a valid token", async () => {
+  const accessTokenExpiryDate = new Date(new Date().getTime() + 3600 * 1000)
+  const refreshTokenExpiryDate = new Date(new Date().getTime() + 24 * 3600 * 1000)
+  const str = JSON.stringify({
+    accessToken: "foo",
+    refreshToken: "bar",
+    accessTokenExpiryDate: accessTokenExpiryDate,
+    refreshTokenExpiryDate: refreshTokenExpiryDate
+  })
+  const token = OAuthTokenCoder.decode(str)
+  expect(token.accessToken).toBe("foo")
+  expect(token.refreshToken).toBe("bar")
+  expect(token.accessTokenExpiryDate).toEqual(accessTokenExpiryDate)
+  expect(token.refreshTokenExpiryDate).toEqual(refreshTokenExpiryDate)
+})
+
 test("It throws an error when the returned OAuth token does not contain an access token", async () => {
   const str = JSON.stringify({
     refreshToken: "bar",
