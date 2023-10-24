39efb473b813587df68fe3bad5e364f4332197b5
Simon Støvring <mail@simonbs.dk>
2023-10-24 15:25:16 +0200

    Fixes typos

--- src/common/userData/Auth0SessionDataRepository.ts
+++ src/common/userData/Auth0SessionDataRepository.ts
@@ -12,7 +12,7 @@ export default class Auth0SessionDataRepository<T> implements ISessionDataReposi
   async get(): Promise<T | null> {
     const session = await getSession()
     if (!session) {
-      throw new Error(`User data could not be read because the user is not authenticated."`)
+      throw new Error(`User data could not be read because the user is not authenticated.`)
     }
     return await this.repository.get(session.user.sub)
   }
@@ -20,7 +20,7 @@ export default class Auth0SessionDataRepository<T> implements ISessionDataReposi
   async set(value: T): Promise<void> {
     const session = await getSession()
     if (!session) {
-      throw new Error(`User data could not be persisted because the user is not authenticated."`)
+      throw new Error(`User data could not be persisted because the user is not authenticated.`)
     }
     return await this.repository.set(session.user.sub, value)
   }
@@ -28,7 +28,7 @@ export default class Auth0SessionDataRepository<T> implements ISessionDataReposi
   async delete(): Promise<void> {
     const session = await getSession()
     if (!session) {
-      throw new Error(`User data could not be deleted because the user is not authenticated."`)
+      throw new Error(`User data could not be deleted because the user is not authenticated.`)
     }
     return await this.repository.delete(session.user.sub)
   }
