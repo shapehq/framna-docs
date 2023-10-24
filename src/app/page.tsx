c951541df8224a30a67ddea768aa711aeee540e5
Simon Støvring <mail@simonbs.dk>
2023-10-24 10:59:04 +0200

    Moves ProjectsPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import SessionOAuthTokenBarrier from "@/features/auth/view/SessionOAuthTokenBarrier"
-import ProjectsPage from "@/features/projects/view/ProjectsPage"
+import ProjectsPage from "@/features/projects/view/client/ProjectsPage"
 
 export default async function Page() {
   return (
