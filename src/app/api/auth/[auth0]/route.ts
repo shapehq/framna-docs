76e6f7814a9562256764ee369c474d0af0796bbb
Simon Støvring <mail@simonbs.dk>
2023-10-24 11:29:38 +0200

    Constructs URL from SHAPE_DOCS_BASE_URL (#47)

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -13,14 +13,15 @@ import {
   sessionOAuthTokenRepository
 } from "@/composition"
 
+const { SHAPE_DOCS_BASE_URL } = process.env
+
 const afterCallback: AfterCallbackAppRoute = async (_req, session) => {
   await initialOAuthTokenService.fetchInitialAuthTokenForUser(session.user.sub)
   return session
 }
 
-const onError: AppRouterOnError = async (req: NextRequest) => {
-  const url = req.nextUrl.clone()
-  url.pathname = "/api/auth/forceLogout"
+const onError: AppRouterOnError = async () => {
+  const url = new URL(SHAPE_DOCS_BASE_URL + "/api/auth/forceLogout")
   return NextResponse.redirect(url)
 }
 
