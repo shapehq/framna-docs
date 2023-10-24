f57b63a27749190168d22ed189b45dd0ca89e61e
Simon Støvring <mail@simonbs.dk>
2023-10-24 11:26:24 +0200

    Adds unit tests for URL utils (#46)
    * Restructures project
    
    * Moves theme
    
    * Moves ProjectsPage
    
    * Moves URL utils
    
    * Moves fetcher
    
    * Adds unit tests for URL utils

--- src/common/utils/url.ts
+++ src/common/utils/url.ts
@@ -12,7 +12,7 @@ export function getProjectId(url?: string) {
 }
 
 function getVersionAndSpecification(url?: string) {
-    const project = getProjectId(url)?.replace('-openapi', '');
+    const project = getProjectId(url)
     if (url && project) {
         const versionAndSpecification = url.substring(project.length + 2)// remove first slash
         let specification: string | undefined = undefined;
