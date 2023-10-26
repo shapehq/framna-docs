1280c8db429453d534e8f9fbfce180c70493bcf5
Simon Støvring <mail@simonbs.dk>
2023-10-26 11:39:54 +0200

    Fixes clicking duck to go home

--- src/features/sidebar/view/SidebarHeader.tsx
+++ src/features/sidebar/view/SidebarHeader.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image"
 import { Stack } from "@mui/material"
+import Link from "next/link"
 
 export default function SidebarHeader() {
   return (
     <Stack direction="row" alignItems="center" spacing={1}>
-      <Image src="/duck.png" alt="Duck" width={40} height={45} priority={true}/>
+      <Link href={"/"}>
+        <Image src="/duck.png" alt="Duck" width={40} height={45} priority={true}/>
+      </Link>
     </Stack>
   )
 }
