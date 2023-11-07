77b450b11fea32ac59c382aafa6a79ac59c0850d
Simon Støvring <mail@simonbs.dk>
2023-11-07 21:52:50 +0100

    Fixes imports

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,4 +1,4 @@
-export type * from "./fetcher"
-export type { default as fetcher } from "./fetcher"
+export * from "./fetcher"
+export { default as fetcher } from "./fetcher"
 export * from "./url"
 export { default as ZodJSONCoder } from "./ZodJSONCoder"
