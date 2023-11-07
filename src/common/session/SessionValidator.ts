f08d6adb6f09e6fdb7c27560cad52883539d778a
Simon Støvring <mail@simonbs.dk>
2023-11-07 12:57:29 +0100

    Adds tests for SessionValidator

--- src/common/session/SessionValidator.ts
+++ src/common/session/SessionValidator.ts
@@ -1,25 +1,28 @@
-import ISession from "./ISession"
 import ISessionValidator from "./ISessionValidator"
 
+interface IIsGuestReader {
+  getIsGuest(): Promise<boolean>
+}
+
 type SessionValidatorConfig = {
-  readonly session: ISession
+  readonly isGuestReader: IIsGuestReader
   readonly guestSessionValidator: ISessionValidator
   readonly hostSessionValidator: ISessionValidator
 }
 
 export default class SessionValidator implements ISessionValidator {
-  private readonly session: ISession
+  private readonly isGuestReader: IIsGuestReader
   private readonly guestSessionValidator: ISessionValidator
   private readonly hostSessionValidator: ISessionValidator
   
   constructor(config: SessionValidatorConfig) {
-    this.session = config.session
+    this.isGuestReader = config.isGuestReader
     this.guestSessionValidator = config.guestSessionValidator
     this.hostSessionValidator = config.hostSessionValidator
   }
   
   async validateSession(): Promise<boolean> {
-    const isGuest = await this.session.getIsGuest()
+    const isGuest = await this.isGuestReader.getIsGuest()
     if (isGuest) {
       return await this.guestSessionValidator.validateSession()
     } else {
