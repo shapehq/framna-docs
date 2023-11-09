b66e0bcb8d324a60ae1529d4614f9112ec43fe0f
Simon Støvring <mail@simonbs.dk>
2023-11-09 18:40:27 +0100

    Adds missing delete function

--- src/features/auth/domain/accessToken/GuestAccessTokenRepository.ts
+++ src/features/auth/domain/accessToken/GuestAccessTokenRepository.ts
@@ -1,6 +1,7 @@
 export interface IRepository {
   get(userId: string): Promise<string | null>
   setExpiring(userId: string, token: string, timeToLive: number): Promise<void>
+  delete(userId: string): Promise<void>
 }
 
 export default class GuestAccessTokenRepository {
@@ -16,5 +17,9 @@ export default class GuestAccessTokenRepository {
   
   async set(userId: string, accessToken: string): Promise<void> {
     await this.repository.setExpiring(userId, accessToken, 7 * 24 * 3600)
+  }
+  
+  async delete(userId: string): Promise<void> {
+    await this.repository.delete(userId)
   }
 }
