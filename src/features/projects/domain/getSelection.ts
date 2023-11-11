b23f284c7c2ee64c86fad0ef3556d8d7ed27435f
Simon Støvring <mail@simonbs.dk>
2023-11-11 16:05:14 +0100

    Removes logic that belongs to the introduction of remote versions

--- src/features/projects/domain/getSelection.ts
+++ src/features/projects/domain/getSelection.ts
@@ -30,24 +30,8 @@ export default function getSelection({
     return {}
   }
   let version: Version | undefined
-  let didMoveSpecificationIdToVersionId = false
   if (versionId) {
     version = project.versions.find(e => e.id == versionId)
-    if (!version && specificationId && !isSpecificationIdFilename(specificationId)) {
-      // With the introduction of remote versions that are specified in the .shape-docs.yml
-      // configuration file, it has become impossible to tell if the last component in a URL
-      // is the specification ID or if it belongs to the version ID. Previously, we required
-      // specification IDs to end with either ".yml" or ".yaml" but that no longer makes
-      // sense when users can define versions.
-      // Instead we assume that the last component is the specification ID but if we cannot
-      // find a version with what we then believe to be the version ID, then we attempt to
-      // finding a version with the ID `{versionId}/{specificationId}` and if that succeeds,
-      // we select the first specification in that version by flagging that the ID of the
-      // specification is considered part of the version ID.
-      const longId = [versionId, specificationId].join("/")
-      version = project.versions.find(e => e.id == longId)
-      didMoveSpecificationIdToVersionId = version != undefined
-    }
   } else if (project.versions.length > 0) {
     version = project.versions[0]
   }
@@ -55,7 +39,7 @@ export default function getSelection({
     return { project }
   }
   let specification: OpenApiSpecification | undefined
-  if (specificationId && !didMoveSpecificationIdToVersionId) {
+  if (specificationId) {
     specification = version.specifications.find(e => e.id == specificationId)
   } else if (version.specifications.length > 0) {
     specification = version.specifications[0]
@@ -80,8 +64,4 @@ function guessSelection(pathname: string) {
     const specificationId = comps[comps.length - 1]
     return { projectId, versionId, specificationId }
   }
-}
-
-function isSpecificationIdFilename(specificationId: string): boolean {
-  return specificationId.endsWith(".yml") || specificationId.endsWith(".yaml")
 }
