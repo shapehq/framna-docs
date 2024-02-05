296a251f0206968384f50002d6093dd6bd715a8d
Simon Støvring <mail@simonbs.dk>
2024-02-05 16:38:34 +0100

    Removes unneeded unit tests

--- __test__/auth/IsUserGuestReader.test.ts
+++ __test__/auth/IsUserGuestReader.test.ts
@@ -1,22 +1,11 @@
 import { IsUserGuestReader } from "../../src/features/auth/domain"
-import { UserIdentityProvider } from "../../src/features/auth/domain"
 
-test("It does not consider a user to be a guest if they are logged in with GitHub", async () => {
-  const sut = new IsUserGuestReader({
-    async getUserIdentityProvider() {
-      return UserIdentityProvider.GITHUB
-    }
-  })
-  const isGuest = await sut.getIsUserGuest("foo")
-  expect(isGuest).toBeFalsy()
-})
-
-test("It considers user a to be a guest if they are logged in with username and password", async () => {
-  const sut = new IsUserGuestReader({
-    async getUserIdentityProvider() {
-      return UserIdentityProvider.USERNAME_PASSWORD
-    }
-  })
-  const isGuest = await sut.getIsUserGuest("foo")
-  expect(isGuest).toBeTruthy()
+test("It considers no user to be a guest", async () => {
+  const sut = new IsUserGuestReader()
+  const isGuestA = await sut.getIsUserGuest("foo")
+  const isGuestB = await sut.getIsUserGuest("bar")
+  const isGuestC = await sut.getIsUserGuest("hello")
+  expect(isGuestA).toBeFalsy()  
+  expect(isGuestB).toBeFalsy()
+  expect(isGuestC).toBeFalsy()
 })
