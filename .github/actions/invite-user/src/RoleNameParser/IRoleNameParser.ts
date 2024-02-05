9b0bbfab903cc495d22fb9ac89d54dc325be5f18
Simon Støvring <mail@simonbs.dk>
2024-02-05 15:58:40 +0100

    Removes Auth0-related GitHub actions

--- .github/actions/invite-user/src/RoleNameParser/IRoleNameParser.ts
+++
@@ -1,3 +0,0 @@
-export default interface IRoleNameParser {
-  parse(roleNames: string): string[]
-}
--- .github/actions/invite-user/src/RoleNameParser/RoleNameParser.ts
+++
@@ -1,9 +0,0 @@
-import IRoleNameParser from "./IRoleNameParser"
-
-export default class RoleNameParser implements IRoleNameParser {
-  parse(roleNames: string): string[] {
-    return roleNames.split(",")
-      .map(e => e.trim().toLowerCase())
-      .filter(e => e.length > 0)
-  }
-}
