d65080e03b00b61371ef1f33e71c16201a06f7b4
Ulrik Andersen <ulrik@shape.dk>
2024-04-29 12:55:47 +0200

    Basic styling of admin page

--- src/features/admin/view/AdminPage.tsx
+++ src/features/admin/view/AdminPage.tsx
@@ -1,5 +1,5 @@
 import { guestInviter, guestRepository } from "@/composition"
-import { Button, ButtonGroup, Chip, FormGroup, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from "@mui/material"
+import { Box, Button, ButtonGroup, Chip, FormGroup, Paper, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from "@mui/material"
 import Table from "@mui/material/Table"
 import { revalidatePath } from "next/cache"
 
@@ -36,47 +36,59 @@ const AdminPage = async () => {
 
     return (
         <>
-        <h1>Guest Admin</h1>
-        
-        <h2>Invite Guest</h2>
-        <form action={sendInvite}>
-            <FormGroup row={true}>
-                <TextField name="email" type="email" placeholder="Email" required />
-                <TextField name="projects" placeholder="Projects" />
-                <Button type="submit" variant="outlined">Send invite</Button>
-            </FormGroup>
-        </form>
-        
-        <h2>Guests</h2>
-        <TableContainer>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Email</TableCell>
-                        <TableCell>Status</TableCell>
-                        <TableCell>Projects</TableCell>
-                        <TableCell>Actions</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {guests.map((row, index) => (
-                        <TableRow key={index}>
-                            <TableCell>{row.email}</TableCell>
-                            <TableCell><Chip label={row.status} color={row.status=="active" ? "primary" : "default"}/></TableCell>
-                            <TableCell>{row.projects.join(", ")}</TableCell>
-                            <TableCell>
-                                <ButtonGroup variant="outlined">
-                                    <form action={removeGuest}>
-                                        <input type="hidden" name="email" value={row.email} />
-                                        <Button type="submit">Remove</Button>
-                                    </form>
-                                </ButtonGroup>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <Box sx={{ p: 2 }}>
+            <Typography variant="h4" gutterBottom>Admin</Typography>
+
+            <Paper sx={{ p: 2, my: 4 }} elevation={2} >
+                <Typography variant="h6" gutterBottom>Invite Guest</Typography>
+                <Typography variant="body2" gutterBottom>
+                    Invite a guest to access the platform. An invitation email will be sent to the provided email address.
+                </Typography>
+                <form action={sendInvite}>
+                    <FormGroup row={true}>
+                        <TextField name="email" type="email" placeholder="Email" required sx={{ mr: 1 }} />
+                        <TextField name="projects" placeholder="Projects" sx={{ mr: 1 }} />
+                        <Button type="submit" variant="outlined">Send invite</Button>
+                    </FormGroup>
+                </form>
+            </Paper>
+            <Typography variant="h6" gutterBottom>Guests</Typography>
+
+            <Typography variant="body2" gutterBottom>
+                Manage guests who have access to the platform.
+            </Typography>
+            <Paper>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Email</TableCell>
+                                <TableCell>Status</TableCell>
+                                <TableCell>Projects</TableCell>
+                                <TableCell>Actions</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {guests.map((row, index) => (
+                                <TableRow key={index}>
+                                    <TableCell>{row.email}</TableCell>
+                                    <TableCell><Chip label={row.status} color={row.status=="active" ? "primary" : "default"}/></TableCell>
+                                    <TableCell>{row.projects.join(", ")}</TableCell>
+                                    <TableCell>
+                                        <ButtonGroup variant="outlined">
+                                            <form action={removeGuest}>
+                                                <input type="hidden" name="email" value={row.email} />
+                                                <Button type="submit">Remove</Button>
+                                            </form>
+                                        </ButtonGroup>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+        </Box>
         </>
     )
 }
