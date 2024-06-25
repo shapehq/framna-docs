b44ceb7b927e584bf487e6ab82211b589080599f
Ulrik Andersen <ulrik@shape.dk>
2024-06-25 09:55:00 +0200

    Add placeholder edit button

---
+++ src/features/admin/view/EditGuestForm.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Button } from "@mui/material";
+
+export const EditGuestForm = () => {
+    return (
+        <form>
+            <Button>Edit</Button>
+        </form>
+    )
+}
