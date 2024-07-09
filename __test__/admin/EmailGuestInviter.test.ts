cb1eb4eb205bdb0607ea62d5aea6ccc5e965eb24
Simon Støvring <mail@simonbs.dk>
2024-07-09 08:55:30 +0200

    Streamlines indentation

--- __test__/admin/EmailGuestInviter.test.ts
+++ __test__/admin/EmailGuestInviter.test.ts
@@ -1,57 +1,57 @@
 import nodemailer from "nodemailer"
 import { EmailGuestInviter } from "../../src/features/admin/data"
 
-jest.mock('nodemailer', () => {
-    return {
-        createTransport: jest.fn().mockReturnValue({
-            sendMail: jest.fn(),
-        }),
-    }
+jest.mock("nodemailer", () => {
+  return {
+    createTransport: jest.fn().mockReturnValue({
+      sendMail: jest.fn()
+    })
+  }
 })
 
-describe('EmailGuestInviter', () => {
-    describe('constructor', () => {
-        it('should create a transporter', () => {
-            const sut = new EmailGuestInviter({
-                url: "https://docs.shapetools.io",
-                server: {
-                    host: 'smtp.example.com',
-                    user: 'user',
-                    pass: 'pass',
-                },
-                from: 'some@email.dk',
-            })
+describe("EmailGuestInviter", () => {
+  describe("constructor", () => {
+    it("should create a transporter", () => {
+      const sut = new EmailGuestInviter({
+        url: "https://docs.shapetools.io",
+        server: {
+          host: "smtp.example.com",
+          user: "user",
+          pass: "pass"
+        },
+        from: "some@email.dk"
+      })
 
-            expect(nodemailer.createTransport).toHaveBeenCalledWith({
-                host: 'smtp.example.com',
-                auth: {
-                    user: 'user',
-                    pass: 'pass',
-                },
-            })
-        })
+      expect(nodemailer.createTransport).toHaveBeenCalledWith({
+        host: "smtp.example.com",
+        auth: {
+          user: "user",
+          pass: "pass"
+        }
+      })
     })
+  })
 
-    describe('inviteGuestByEmail', () => {
-        it('should send an invite', async () => {
-            const sut = new EmailGuestInviter({
-                url: "https://docs.shapetools.io",
-                server: {
-                    host: 'smtp.example.com',
-                    user: 'user',
-                    pass: 'pass',
-                },
-                from: 'some@email.dk',
-            })
-    
-            sut.inviteGuestByEmail('guest@email.dk')
-    
-            expect(nodemailer.createTransport().sendMail).toHaveBeenCalledWith({
-                to: 'guest@email.dk',
-                from: 'some@email.dk',
-                subject: 'You have been invited to join Shape Docs',
-                html: expect.any(String), // difficult to test the exact content
-            });
-        })
+  describe("inviteGuestByEmail", () => {
+    it("should send an invite", async () => {
+      const sut = new EmailGuestInviter({
+        url: "https://docs.shapetools.io",
+        server: {
+          host: "smtp.example.com",
+          user: "user",
+          pass: "pass"
+        },
+        from: "some@email.dk"
+      })
+  
+      sut.inviteGuestByEmail("guest@email.dk")
+  
+      expect(nodemailer.createTransport().sendMail).toHaveBeenCalledWith({
+        to: "guest@email.dk",
+        from: "some@email.dk",
+        subject: "You have been invited to join Shape Docs",
+        html: expect.any(String) // difficult to test the exact content
+      })
     })
+  })
 })
