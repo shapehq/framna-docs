dcb2fba2702a6a853307972079c30c98f168d3a5
Simon Støvring <mail@simonbs.dk>
2024-07-25 13:25:58 +0200

    Improves naming

--- src/features/projects/domain/FilteringGitHubRepositoryDataSource.ts
+++ src/features/projects/domain/FilteringGitHubRepositoryDataSource.ts
@@ -5,23 +5,23 @@ import { splitOwnerAndRepository } from "@/common"
 
 export default class FilteringGitHubRepositoryDataSource implements IGitHubRepositoryDataSource {
   private readonly dataSource: IGitHubRepositoryDataSource
-  private readonly hiddenRepositories: string[]
+  private readonly rawHiddenRepositories: string[]
   
   constructor(config: {
     dataSource: IGitHubRepositoryDataSource,
     hiddenRepositories: string[]
   }) {
     this.dataSource = config.dataSource
-    this.hiddenRepositories = config.hiddenRepositories
+    this.rawHiddenRepositories = config.hiddenRepositories
   }
   
   async getRepositories(): Promise<GitHubRepository[]> {
     const repositories = await this.dataSource.getRepositories()
-    const hiddenOwnerAndRepoNameList = this.hiddenRepositories
+    const hiddenRepositories = this.rawHiddenRepositories
       .map(splitOwnerAndRepository)
       .filter(e => e !== undefined)
     return repositories.filter(repository => {
-      const hiddenMatch = hiddenOwnerAndRepoNameList.find(e => 
+      const hiddenMatch = hiddenRepositories.find(e => 
         e.owner == repository.owner && e.repository == repository.name
       )
       return hiddenMatch === undefined
