d18a6e27ba4bc295ea85891b495aa65db5d64ace
Simon Støvring <mail@simonbs.dk>
2024-07-26 14:55:29 +0200

    Hides header when no project is selected

--- src/app/(authed)/(home)/[[...slug]]/layout.tsx
+++ src/app/(authed)/(home)/[[...slug]]/layout.tsx
@@ -1,8 +1,15 @@
+"use client"
+
 import SecondarySplitHeader from "@/features/sidebar/view/SecondarySplitHeader"
 import TrailingToolbarItem from "@/features/projects/view/toolbar/TrailingToolbarItem"
 import MobileToolbar from "@/features/projects/view/toolbar/MobileToolbar"
+import { useProjectSelection } from "@/features/projects/data"
 
 export default function Page({ children }: { children: React.ReactNode }) {
+  const { project } = useProjectSelection()
+  if (!project) {
+    return <></>
+  }
   return (
     <>
       <SecondarySplitHeader mobileToolbar=<MobileToolbar/>>
