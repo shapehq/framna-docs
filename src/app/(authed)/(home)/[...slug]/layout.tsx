83c99d678c845674fbc725ac1a4aa60c11b119e4
mpabarca <maria@shape.dk>
2024-08-09 14:45:57 +0200

    Create Not Found page

--- src/app/(authed)/(home)/[...slug]/layout.tsx
+++ src/app/(authed)/(home)/[...slug]/layout.tsx
@@ -4,11 +4,12 @@ import SecondarySplitHeader from "@/features/sidebar/view/SecondarySplitHeader"
 import TrailingToolbarItem from "@/features/projects/view/toolbar/TrailingToolbarItem"
 import MobileToolbar from "@/features/projects/view/toolbar/MobileToolbar"
 import { useProjectSelection } from "@/features/projects/data"
+import NotFound from "@/features/projects/view/NotFound"
 
 export default function Page({ children }: { children: React.ReactNode }) {
   const { project } = useProjectSelection()
   if (!project) {
-    return <></>
+    return <NotFound />
   }
   return (
     <>
