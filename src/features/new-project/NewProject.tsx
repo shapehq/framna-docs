bb7f56635c6a954d7b57879ff41925c84b6f2cee
Simon Støvring <mail@simonbs.dk>
2024-08-15 20:19:56 +0200

    Removes name from title

--- src/features/new-project/NewProject.tsx
+++ src/features/new-project/NewProject.tsx
@@ -1,13 +1,8 @@
-import {
-  Box,
-} from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import NewProjectSteps from "./view/NewProjectSteps"
 import { env } from "@/common"
 import MessageLinkFooter from "@/common/ui/MessageLinkFooter"
-import HighlightText from "@/common/ui/HighlightText"
-import { BASE_COLORS } from "@/common/theme/theme"
 
-const SITE_NAME = env.getOrThrow("NEXT_PUBLIC_SHAPE_DOCS_TITLE")
 const HELP_URL = env.get("NEXT_PUBLIC_SHAPE_DOCS_HELP_URL")
 
 interface NewProjectProps {
@@ -21,8 +16,6 @@ const NewProject = ({
   templateName,
   ownerRepository
 }: NewProjectProps) => {
-  const title = "Add a new project to "
-
   return (
     <Box
       display="flex"
@@ -33,13 +26,9 @@ const NewProject = ({
       width={1}
       gap={6}
     >
-      <HighlightText
-        content={`${title} ${SITE_NAME}`}
-        highlight={SITE_NAME}
-        color={BASE_COLORS[0]}
-        variant="h4"
-        isSolidOpacity
-      />
+      <Typography variant="h4">
+        Add a new project
+      </Typography>
       <NewProjectSteps 
         repositoryNameSuffix={repositoryNameSuffix}
         templateName={templateName}
