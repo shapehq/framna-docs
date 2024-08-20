5f78c982d3466b122aedd6593b3d7d0c65c1d490
Simon Støvring <mail@simonbs.dk>
2024-08-20 23:25:47 +0200

    Improves New Project button avatar

--- src/features/sidebar/view/internal/sidebar/NewProjectButton.tsx
+++ src/features/sidebar/view/internal/sidebar/NewProjectButton.tsx
@@ -5,19 +5,28 @@ import { Stack, List } from "@mui/material"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPlus } from "@fortawesome/free-solid-svg-icons"
 import { Template as ProjectListItemTemplate } from "./projects/ProjectListItem"
+import { Squircle as ProjectAvatarSquircle } from "./projects/ProjectAvatar"
+import { useTheme } from "@mui/material/styles"
 
 const NewProjectButton = () => {
   const router = useRouter()
   const pathname = usePathname()
+  const theme = useTheme()
   return (
     <List disablePadding>
       <ProjectListItemTemplate
         title="New Project"
         selected={pathname === "/new"}
         avatar={
-          <Stack sx={{ height: "100%" }} justifyContent="center">
-            <FontAwesomeIcon icon={faPlus} size="lg" />
-          </Stack>
+          <ProjectAvatarSquircle
+            width={40}
+            height={40}
+            sx={{ background: "#dfdfdf" }}
+            >
+            <Stack sx={{ height: "100%" }} justifyContent="center">
+              <FontAwesomeIcon icon={faPlus} size="lg" color="#bababa" />
+            </Stack>
+          </ProjectAvatarSquircle>
         }
         onSelect={() => {
           router.push("/new")
