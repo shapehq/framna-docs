7aee17f396a4acbc33c2627604dba389ea086462
mpabarca <maria@shape.dk>
2024-09-04 11:20:22 +0200

    Create SecondaryHeaderPlaceholder component

---
+++ src/features/sidebar/view/SecondarySplitHeaderPlaceholder.tsx
@@ -0,0 +1,49 @@
+import { Box, Stack, Divider, Skeleton, Typography } from "@mui/material";
+import React from "react";
+
+export const HEIGHT_HEADER = 80;
+
+const SecondaryHeaderPlaceholder = () => {
+  const skeletonCount = 4;
+
+  return (
+    <Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          padding: 2,
+          paddingLeft: { xs: 4, md: 2 },
+          maxWidth: "1460px",
+          margin: "auto",
+          height: HEIGHT_HEADER,
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexGrow: 1,
+            justifyContent: "end",
+            paddingRight: "30px",
+          }}
+        >
+          <Stack direction="row" alignItems="center" gap={2}>
+            {Array.from({ length: skeletonCount }, (_, index) => (
+              <React.Fragment key={index}>
+                <Skeleton variant="rounded" width={70} height={30} />
+                {index < skeletonCount - 1 && (
+                  <Typography variant="h6" sx={{ marginRight: 1 }}>
+                    /
+                  </Typography>
+                )}
+              </React.Fragment>
+            ))}
+          </Stack>
+        </Box>
+      </Box>
+      <Divider />
+    </Box>
+  );
+};
+
+export default SecondaryHeaderPlaceholder;
