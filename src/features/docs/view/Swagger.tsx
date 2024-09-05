e3adc462cca501b4f096eaa64b375203cff026d3
mpabarca <maria@shape.dk>
2024-09-05 14:50:22 +0200

    Revert "Add delay and debounce to loader on Swagger Viewer"
    This reverts commit 12a4dd9e36cb7282ddcdee41167520b655b53957.

--- src/features/docs/view/Swagger.tsx
+++ src/features/docs/view/Swagger.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react"
+import { useState } from "react"
 import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
 import { Box } from "@mui/material"
@@ -6,36 +6,10 @@ import LoadingWrapper from "./LoadingWrapper"
 
 const Swagger = ({ url }: { url: string }) => {
   const [isLoading, setLoading] = useState(true)
-  const debounceTimeout = useRef<NodeJS.Timeout | null>(null)
-
-  const handleComplete = () => {
-    // Clear any existing debounce timeout
-    if (debounceTimeout.current) {
-      clearTimeout(debounceTimeout.current)
-    }
-    // Set a debounce delay to wait before updating the loading state
-    debounceTimeout.current = setTimeout(() => {
-      setLoading(false)
-    }, 1200)
-  }
-  // Cleanup timeout on unmount
-  useEffect(() => {
-    return () => {
-      if (debounceTimeout.current) {
-        clearTimeout(debounceTimeout.current)
-      }
-    }
-  }, [])
-
   return (
     <LoadingWrapper showLoadingIndicator={isLoading}>
       <Box sx={{ paddingBottom: 1 }}>
-        <SwaggerUI
-          url={url}
-          onComplete={handleComplete}
-          deepLinking
-          persistAuthorization
-        />
+        <SwaggerUI url={url} onComplete={() => setLoading(false)} deepLinking persistAuthorization />
       </Box>
     </LoadingWrapper>
   )
