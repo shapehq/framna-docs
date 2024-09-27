720794ec24f9a77043d3b89e0256c749c57ac361
Simon Støvring <mail@simonbs.dk>
2024-09-27 21:31:45 +0200

    Delays error messages until data is refreshed

--- src/common/context/ProjectsContext.ts
+++ src/common/context/ProjectsContext.ts
@@ -6,11 +6,13 @@ import { Project } from "@/features/projects/domain"
 export const SidebarTogglableContext = createContext<boolean>(true)
 
 type ProjectsContextValue = {
+  cached: boolean,
   projects: Project[],
   setProjects: (projects: Project[]) => void
 }
 
 export const ProjectsContext = createContext<ProjectsContextValue>({
+  cached: true,
   projects: [],
   setProjects: () => {}
 })
