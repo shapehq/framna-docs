ed7fa81f94f5db6552f7b4c4dd89877eaf2f6521
mpabarca <maria@shape.dk>
2024-10-04 16:29:07 +0200

    Add refreshed state to ProjectsContext to show updated data of projects and branches

--- src/common/context/ProjectsContext.ts
+++ src/common/context/ProjectsContext.ts
@@ -6,13 +6,13 @@ import { Project } from "@/features/projects/domain"
 export const SidebarTogglableContext = createContext<boolean>(true)
 
 type ProjectsContextValue = {
-  cached: boolean,
+  refreshed: boolean,
   projects: Project[],
   setProjects: (projects: Project[]) => void
 }
 
 export const ProjectsContext = createContext<ProjectsContextValue>({
-  cached: true,
+  refreshed: false,
   projects: [],
   setProjects: () => {}
 })
