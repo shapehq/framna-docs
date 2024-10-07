1bfdcc2a134d63f1532955ee101a52dad3d5cfc6
mpabarca <maria@shape.dk>
2024-10-07 15:27:12 +0200

    Fix show SecondaryHeaderPlaceholder in Not Found page

--- src/app/(authed)/(project-doc)/[...slug]/layout.tsx
+++ src/app/(authed)/(project-doc)/[...slug]/layout.tsx
@@ -4,36 +4,33 @@ import { Box, Stack } from "@mui/material"
 import { useTheme } from "@mui/material/styles"
 import TrailingToolbarItem from "@/features/projects/view/toolbar/TrailingToolbarItem"
 import MobileToolbar from "@/features/projects/view/toolbar/MobileToolbar"
-import { useProjectSelection } from "@/features/projects/data"
 import SecondaryHeaderPlaceholder from "@/features/sidebar/view/SecondarySplitHeaderPlaceholder"
-import SecondarySplitHeader from "@/features/sidebar/view/SecondarySplitHeader"
 import { useContext } from "react"
 import { ProjectsContext } from "@/common"
 import LoadingIndicator from "@/common/ui/LoadingIndicator"
+import SecondarySplitHeader from "@/features/sidebar/view/SecondarySplitHeader"
 
 export default function Page({ children }: { children: React.ReactNode }) {
-  const { project } = useProjectSelection()
   const { refreshed } = useContext(ProjectsContext)
 
   const theme = useTheme()
   
   return (
     <Stack sx={{ height: "100%" }}>
-      {refreshed ? 
         <>
-          {project &&
+          {!refreshed ? <SecondaryHeaderPlaceholder/> :
             <SecondarySplitHeader mobileToolbar={<MobileToolbar/>}>
               <TrailingToolbarItem/>
             </SecondarySplitHeader>
           }
-          {!project && <SecondaryHeaderPlaceholder/>}
           <Box sx={{ height: "0.5px", background: theme.palette.divider }} />
-          <main style={{ flexGrow: "1", overflowY: "auto" }}>
-            {children}
-          </main>
+          {refreshed ? 
+            <main style={{ flexGrow: "1", overflowY: "auto" }}>
+              {children}
+            </main> : 
+            <LoadingIndicator />
+          }
         </>
-      : <LoadingIndicator />
-    }
     </Stack>
   )
 }
