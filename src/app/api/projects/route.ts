bc99998cd3920f00c0e2a8cefd6e49b4812ad3ad
Oscar <albertlund121@gmail.com>
2025-09-29 12:58:52 +0200

    post til refreshProjects

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -4,7 +4,7 @@ import { projectDataSource } from "@/composition";
 
 export async function GET() {
   const projects = await projectDataSource.getProjects()
-  return NextResponse.json({ projects})
+  return NextResponse.json({ projects })
 }
 
 export async function POST() {
