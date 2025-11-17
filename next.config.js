eee937136e81e2a9d3607d9507ef6dbcd7a8f097
Simon Støvring <mail@simonbs.dk>
2025-11-17 10:25:41 +0100

    Removes eslint from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Allows production builds to successfully complete even if it has linting errors.
-    // This is only OK because we do linting as part of our CI setup.
-    ignoreDuringBuilds: true,
-  },
   // Output standalone to be used for Docker builds.
   output: 'standalone',
 }
